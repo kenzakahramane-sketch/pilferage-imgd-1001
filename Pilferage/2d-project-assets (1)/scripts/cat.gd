738821ec23736e408a9b3c4dd512349514ebcba3
The1v1Master <dchan@wpi.edu>
2026-09-15 02:31:03 -0400

    Sidescroller dust particles
    When you move fast enough, a trial of dust particles are left behind.

--- Pilferage/2d-project-assets (1)/scripts/cat.gd
+++ Pilferage/2d-project-assets (1)/scripts/cat.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
 
-const ACCELERATION = 10
+const ACCELERATION = 6
 const JUMP_VELOCITY = -250.0
 const MAX_SPEED = 150
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var hitbox = $CollisionShape2D
+@onready var particles = $CPUParticles2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,10 +15,11 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
+		animated_sprite.play("jumpCool")
 		if abs (velocity.x) < 100:
 			velocity.y = JUMP_VELOCITY
 		else:
-			velocity.y = JUMP_VELOCITY * sqrt((abs(velocity.x) / 100))
+			velocity.y = JUMP_VELOCITY + JUMP_VELOCITY * 0.15 * ((abs(velocity.x) / 100))
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -34,9 +36,24 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, ACCELERATION)
 	if abs(velocity.x) > MAX_SPEED:
 		if velocity.x > 0:
-			velocity.x = MAX_SPEED
+			velocity.x = move_toward(velocity.x, MAX_SPEED, ACCELERATION)
 		if velocity.x < 0:
-			velocity.x = -1 * MAX_SPEED
+			velocity.x = move_toward(velocity.x, -1 * MAX_SPEED, ACCELERATION)
 	move_and_slide()
-	
+	if is_on_floor():
+		if velocity.x == 0:
+			animated_sprite.play("idleCool")
+			particles.emitting = false
+		else:
+			if abs(velocity.x) >= 150:
+				animated_sprite.play("sprintCool")
+				particles.emitting = true
+			else:
+				animated_sprite.play("runCool")
+				particles.emitting = false
+	else:
+		if velocity.y > 0:
+			animated_sprite.play("fallCool")
+			particles.emitting = false
+		
 		
