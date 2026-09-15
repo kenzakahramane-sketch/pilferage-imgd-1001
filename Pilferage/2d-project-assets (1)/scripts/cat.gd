02818b48397c552dd173e72f53af522360366e15
The1v1Master <dchan@wpi.edu>
2026-09-15 18:43:27 -0400

    Update cat.gd

--- Pilferage/2d-project-assets (1)/scripts/cat.gd
+++ Pilferage/2d-project-assets (1)/scripts/cat.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const ACCELERATION = 3
+const ACCELERATION = 2
 const JUMP_VELOCITY = -250.0
 const MAX_SPEED = 150
 @onready var animated_sprite = $AnimatedSprite2D
@@ -21,10 +21,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump, jumps higher if you build up speed.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		animated_sprite.play("jumpCool")
-		if abs (velocity.x) < 100:
-			velocity.y = JUMP_VELOCITY
-		else:
-			velocity.y = JUMP_VELOCITY + JUMP_VELOCITY * 0.15 * (abs(velocity.x) / 100)
+		velocity.y = JUMP_VELOCITY + JUMP_VELOCITY * 0.15 * (abs(velocity.x) / 100)
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("move_left", "move_right")
@@ -38,14 +35,15 @@ func _physics_process(delta: float) -> void:
 		if direction * velocity.x >= 0:
 			velocity.x += direction * ACCELERATION
 		else:
+			velocity.x = move_toward(velocity.x, 0, ACCELERATION * 3)
 			velocity.x += direction * ACCELERATION
 	else:
-		velocity.x = move_toward(velocity.x, 0, ACCELERATION * 0.5)
+		velocity.x = move_toward(velocity.x, 0, ACCELERATION * 2)
 	if abs(velocity.x) > MAX_SPEED:
 		if velocity.x > 0:
-			velocity.x = move_toward(velocity.x, MAX_SPEED, ACCELERATION * 1.15)
+			velocity.x = move_toward(velocity.x, MAX_SPEED, ACCELERATION * 1.5)
 		if velocity.x < 0:
-			velocity.x = move_toward(velocity.x, -1 * MAX_SPEED, ACCELERATION * 1.15)
+			velocity.x = move_toward(velocity.x, -1 * MAX_SPEED, ACCELERATION * 1.5)
 	move_and_slide()
 	# Detects collision for dangerous things.
 	
