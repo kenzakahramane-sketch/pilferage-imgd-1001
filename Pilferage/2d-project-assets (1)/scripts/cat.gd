c45c5568c69596996964d2d4c374c66dde688fdb
Diana845 <morganhdiana@gmail.com>
2026-09-15 20:25:30 -0400

    push animation

--- Pilferage/2d-project-assets (1)/scripts/cat.gd
+++ Pilferage/2d-project-assets (1)/scripts/cat.gd
@@ -23,3 +23,41 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+<<<<<<< Updated upstream
+=======
+	# Detects collision for dangerous things.
+	
+	# Different animations are played based on the current state of your movement.
+	if is_on_floor():
+		if velocity.x == 0:
+			animated_sprite.play("idleCool")
+		else:
+			if abs(velocity.x) >= MAX_SPEED:
+				animated_sprite.play("sprintCool")
+			else:
+				animated_sprite.play("runCool")
+	else:
+		if velocity.y > 0:
+			animated_sprite.play("fallCool")
+	# The push mechanic
+	if Input.is_action_just_pressed("Push"):
+		if detect_right.is_colliding():
+			if Input.is_action_pressed("move_up"):
+				print("push")
+				animated_sprite.play("pushCool") #Changed this from jump to push but it doesn't play
+				velocity.y = JUMP_VELOCITY
+				velocity.x = MAX_SPEED * -1.25
+			else:
+				velocity.x = MAX_SPEED * -1.75
+		else:
+			if detect_left.is_colliding():
+				if Input.is_action_pressed("move_up"):
+					animated_sprite.play("pushCool") #Changed this from jump to push but it doesn't play 
+					velocity.y = JUMP_VELOCITY
+					velocity.x = MAX_SPEED * 1.25
+				else:
+					velocity.x = MAX_SPEED * 1.75
+	
+		
+		
+>>>>>>> Stashed changes
