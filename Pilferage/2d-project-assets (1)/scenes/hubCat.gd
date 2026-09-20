f79bd1236494dbe39e1ff05c1be2fdf4a9ceada0
Diana845 <morganhdiana@gmail.com>
2026-09-20 17:56:57 -0400

    Added Hub world assets

--- Pilferage/2d-project-assets (1)/scenes/hubCat.gd
+++ Pilferage/2d-project-assets (1)/scenes/hubCat.gd
@@ -11,10 +11,10 @@ func _ready():
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
-	var speed = 600
+	var speed = 100
 
 	if Input.is_action_pressed("hubSprint"):
-		speed = 1000
+		speed = 150
 
 	velocity = direction * speed
 
