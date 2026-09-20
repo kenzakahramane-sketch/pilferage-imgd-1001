ce346c119d330b9f68370b1148f65cceb644f034
BananaBoy1007 <rcasey1@wpi.edu>
2026-09-20 17:37:43 -0400

    Changed Brotato to Hub

--- Pilferage/2d-project-assets (1)/scenes/exit.gd
+++ Pilferage/2d-project-assets (1)/scenes/exit.gd
@@ -6,4 +6,4 @@ func _on_to_hub_body_entered(body: Node2D):
 		call_deferred("_change_to_hub")
 
 func _change_to_hub():
-	get_tree().change_scene_to_file("res://scenes/brotatoclone.tscn")
+	get_tree().change_scene_to_file("res://scenes/hub.tscn")
--- Pilferage/2d-project-assets (1)/scenes/exit_door.gd
+++ Pilferage/2d-project-assets (1)/scenes/exit_door.gd
@@ -2,4 +2,4 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.name == "hubCat":
-		get_tree().change_scene_to_file("res://scenes/brotatoclone.tscn")
+		get_tree().change_scene_to_file("res://scenes/hub.tscn")
