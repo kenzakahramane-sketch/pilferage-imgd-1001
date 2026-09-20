c0cda1f9caf1c7a10a5b85df8299cb9c88345299
BananaBoy1007 <rcasey1@wpi.edu>
2026-09-20 18:24:46 -0400

    Revert "Merge remote-tracking branch 'origin/dianaSprint4'"
    This reverts commit 0e55ecf98e7a6d73c9e43ff26bc16e0d0d5eb74d, reversing
    changes made to ce346c119d330b9f68370b1148f65cceb644f034.

--- Pilferage/2d-project-assets (1)/scenes/hubCat.gd
+++ Pilferage/2d-project-assets (1)/scenes/hubCat.gd
@@ -11,10 +11,10 @@ func _ready():
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
-	var speed = 100
+	var speed = 600
 
 	if Input.is_action_pressed("hubSprint"):
-		speed = 150
+		speed = 1000
 
 	velocity = direction * speed
 
