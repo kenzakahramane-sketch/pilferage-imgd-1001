ff1a5a4595c6bdb246d6348fee22bdf8a3e380f3
The1v1Master <dchan@wpi.edu>
2026-09-21 13:55:45 -0400

    not this again

--- Pilferage/2d-project-assets (1)/scripts/Global.gd
+++ Pilferage/2d-project-assets (1)/scripts/Global.gd
@@ -1,5 +1,10 @@
 extends Node
 
 var spawn_point = ""
+<<<<<<< Updated upstream
 var simplified_controls = true
 var music = true
+=======
+var music = true
+var simplified_controls = true
+>>>>>>> Stashed changes
