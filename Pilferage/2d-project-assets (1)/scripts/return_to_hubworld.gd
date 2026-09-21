b762e4fca6348363c521d0531aceaa1bb6485688
The1v1Master <dchan@wpi.edu>
2026-09-21 15:09:01 -0400

    fixed my branch finally
    holy moly

--- Pilferage/2d-project-assets (1)/scripts/return_to_hubworld.gd
+++ Pilferage/2d-project-assets (1)/scripts/return_to_hubworld.gd
@@ -3,8 +3,11 @@ extends Area2D
 func _on_body_entered (body) -> void:
 	Global.spawn_point = "forest"
 	get_tree().change_scene_to_file("res://scenes/brotatoclone.tscn")
+<<<<<<< Updated upstream
 
 
 func _on_area_entered(area: Area2D) -> void:
 	Global.spawn_point = "forest"
 	get_tree().change_scene_to_file("res://scenes/brotatoclone.tscn")
+=======
+>>>>>>> Stashed changes
