e079ef0d80624838899779fe8b9e8e265bbc79c4
The1v1Master <dchan@wpi.edu>
2026-09-22 03:08:48 -0400

    Cleaning up files
    don't want things to be a big mess

--- Pilferage/2d-project-assets (1)/scenes/entrance.gd
+++
@@ -1,5 +0,0 @@
-extends CollisionShape2D
-
-func _on_entrance_body_entered(body: Node2D):
-	if body.name == "hubCat":
-		get_tree().change_scene_to_file("res://scenes/level_1.tscn")
--- Pilferage/2d-project-assets (1)/scenes/exit.gd
+++
@@ -1,9 +0,0 @@
-extends CollisionShape2D
-
-func _on_to_hub_body_entered(body: Node2D):
-	if body.name == "cat":
-		Global.spawn_point = "forest"
-		call_deferred("_change_to_hub")
-
-func _change_to_hub():
-	get_tree().change_scene_to_file("res://scenes/brotatoclone.tscn")
